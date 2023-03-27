367b65e4d598b36e5f4b2062e3bf979f25a00f03
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2023-03-27 15:27:46 +0100

    use .map to display all employees in data file

--- src/Employee.jsx
+++ src/Employee.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import data from './data'
+import React, {useState} from 'react'
+import {data} from './data'
 
 const Employee = () => {
-    
+    const [employees, setEmployees] = useState(data)
   return (
-    <div>Employee</div>
+    <div>
+        {employees.map((employee) => {
+            const {id, fullName, gender, teamName, designation} =  employee
+            return (
+                <div key={id}>
+                    <h4>{fullName}</h4>
+                    <p>{designation}</p>
+                    <p>{gender}</p>
+                    <p>{teamName}</p>
+                </div>                
+            )
+        })}
+    </div>
   )
 }
 
